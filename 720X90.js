38e7369ec5fbff716eab3453c3fe227806902cac
shurikienimake <114287414+shurikienimake@users.noreply.github.com>
2022-09-24 17:23:54 +0700

    Create 720X90.js

---
+++ 720X90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c853da16437a99d8a68c536b29989664',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/c853da16437a99d8a68c536b29989664/invoke.js"></scr' + 'ipt>');
