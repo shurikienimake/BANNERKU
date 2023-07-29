721d4cd82377474888fddcad6a46a7cee9a4a935
shurikienimake <114287414+shurikienimake@users.noreply.github.com>
2023-07-30 05:55:34 +0700

    Update 720X90.js

--- 720X90.js
+++ 720X90.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/c853da16437a99d8a68c536b29989664/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/c853da16437a99d8a68c536b29989664/invoke.js"></scr' + 'ipt>');
